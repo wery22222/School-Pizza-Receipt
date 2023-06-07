4f54cc7bf94d76226d90de2a93c831b2983f5b7e
Eugeniusz Lewandowski <116173695+wery22222@users.noreply.github.com>
2023-06-07 16:57:42 +0100

    Create main.cpp
    With all the work done in GitHub codespace

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+auto createPriceRef();
+const float receipt(std::string& pizzaType, std::string& pizzaSize);
+
+int main()
+{
+    std::string pizzaSize;
+    std::string pizzaType;
+    std::cout<< "Enter Pizza size (large, medium, small) ";
+    std::cin>>pizzaSize;
+    std::cout<<"Enter Pizza type (Hawaiian, Farmhouse, Margherita, Meat Feast, Caspian Special, Super Supreme) ";
+    std::cin >>pizzaType;
+    double price = 0.0;
+    try
+    {
+         price = receipt(pizzaType, pizzaSize);
+    }
+    catch(std::invalid_argument& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    
+    std::cout<<"The price is £"<<price<<std::endl;
+}
+auto createPriceRef()
+{
+    std::map<std::string, float> priceRef;
+    priceRef["Hawaiian"] = 5.50f;
+    priceRef["Farmhouse"] = 5.50f;
+    priceRef["Margherita"] = 5.50f;
+    priceRef["Meat Feast"] = 7.50f;
+    priceRef["Caspian Special"] = 7.50f;
+    priceRef["Super Supreme"] = 7.50f;
+    return priceRef;
+}
+const float receipt(std::string& pizzaType, std::string& pizzaSize)
+{
+    auto wasFound =false;
+    double price = 0;
+    auto priceRef = createPriceRef();
+    for (auto it:priceRef)
+    {
+        if (it.first == pizzaType)
+        {
+            price+=it.second; 
+            wasFound = true;
+            break;
+        }
+    }
+    if (!wasFound)
+        throw std::invalid_argument("No such Pizza type");
+    if (pizzaSize == "large")
+    {
+        return price*=1.5;
+    }
+    if (pizzaSize  =="small")
+        return price/=2;
+    if (pizzaSize != "medium")
+        throw std::invalid_argument("No such Pizza size");
+    return price;
+}
